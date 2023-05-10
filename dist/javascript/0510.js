6b8b005097e6ea950714956f351ac873d8d70117
Paris <paris150637@gmail.com>
2023-05-10 21:36:23 +0800

    20230510

---
+++ dist/javascript/0510.js
@@ -0,0 +1,10 @@
+let age = 18;
+// let是宣告；age是變數；18是變數的數字
+let firstName = 'David';
+//字串是【""】、【''】有包起來是文字，沒包就是變數
+let lastName = "Lin";
+let country = 'TW';
+
+console.log('(' + country + ') ' + firstName + ' ' + lastName);
+//終端機輸出console.log()
+console.log(`(${country}) ${firstName} ${lastName}`); 
