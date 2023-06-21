8c7e46b0eb6159fb382b2de88ce264c2d5d2ef64
Paris <paris150637@gmail.com>
2023-06-21 21:35:14 +0800

    20230621

--- dist/javascript/computer_layout2.js
+++ dist/javascript/computer_layout2.js
@@ -35,3 +35,42 @@ Vue.createApp({
         this.doLoop();
     }
 }).mount('#app-slider');
+
+
+Vue.createApp({
+    data() {
+        // return {}
+        return {
+            isOpen: false,
+            isRun: false,
+        }
+    },
+    methods: {
+        doToggle() {
+            // console.log(123);
+            if (this.isRun) {
+                return;
+            }
+
+            this.isRun = true;
+            this.isOpen = !this.isOpen;
+
+            let wrap = this.$refs.wrap;
+            let popup = this.$refs.popup;
+
+            if (this.isOpen) {
+                gsap.to(popup, { opacity: 1, display: 'block', duration: 1 });
+                gsap.to(wrap, { opacity: 0, display: 'none', duration: 0 });
+                // popup.style.display = 'block';
+                // wrap.style.display = 'none';
+            } else {
+                gsap.to(popup, { opacity: 0, display: 'none', duration: 0 });
+                gsap.to(wrap, { opacity: 1, display: 'block', duration: 1 });
+            }
+
+            setTimeout(() => {
+                this.isRun = false;
+            }, 1000)
+        }
+    }
+}).mount('#patent-design')
